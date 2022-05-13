5233afe1ef0fa12ac85ef83ee3c1862f4bcc8a9a
teknoarantza <99352288+teknoarantza@users.noreply.github.com>
2022-05-13 08:54:11 +0200

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,37 @@
+let pacman = game.createSprite(2, 2)
+let janaria = game.createSprite(4, 4)
+let mamua = game.createSprite(2, 2)
+pacman.set(LedSpriteProperty.Blink, 100)
+mamua.set(LedSpriteProperty.Brightness, 50)
+janaria.set(LedSpriteProperty.Brightness, 10)
+basic.forever(function () {
+    mamua.move(1)
+    basic.pause(500)
+    mamua.ifOnEdgeBounce()
+})
+basic.forever(function () {
+    basic.pause(400)
+    if (input.acceleration(Dimension.X) > 200) {
+        pacman.change(LedSpriteProperty.X, 1)
+    }
+    if (input.acceleration(Dimension.X) < 200) {
+        pacman.change(LedSpriteProperty.X, -1)
+    }
+    if (input.acceleration(Dimension.Y) > 200) {
+        pacman.change(LedSpriteProperty.Y, 1)
+    }
+    if (input.acceleration(Dimension.Y) < -200) {
+        pacman.change(LedSpriteProperty.Y, -1)
+    }
+})
+basic.forever(function () {
+    if (pacman.isTouching(janaria)) {
+        game.addScore(1)
+        janaria.delete()
+        mamua = game.createSprite(randint(0, 4), randint(0, 4))
+        janaria.set(LedSpriteProperty.Brightness, 10)
+    }
+    if (pacman.isTouching(mamua)) {
+        game.gameOver()
+    }
+})
